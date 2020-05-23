5fd4b4e80fe3a835d7c193741073a6d1e5e3ebd0
Petar <kosaninp@gmail.com>
2020-05-23 11:48:28 +0200

    add different shapes on sides of the central cube

--- src/figures.cpp
+++ src/figures.cpp
@@ -1,5 +1,6 @@
 #include "figures.h"
 #include <GL/glut.h>
+#include <cmath>
 
 void coordinate_axes(int len)
 {
@@ -18,13 +19,55 @@ void coordinate_axes(int len)
     glEnd();
 }
 
-void draw_main_cube()
+void draw_main_cube(float size)
 {
+    // draw square on top of the main cube
+    glColor3f(0, 0, 1);
+    glBegin(GL_POLYGON);
+        glVertex3f(size / 4, size / 2, size / 4);
+        glVertex3f(size / 4, size / 2, -size / 4);
+        glVertex3f(-size /4, size / 2, -size / 4);
+        glVertex3f(-size /4, size / 2, size / 4);
+    glEnd();
+
+
+    // draw square on bottom side of the cube
+    glColor3f(0, 1, 0);
+    glBegin(GL_POLYGON);
+        glVertex3f(size / 4, -size / 2, size / 4);
+        glVertex3f(size / 4, -size / 2, -size / 4);
+        glVertex3f(-size /4, -size / 2, -size / 4);
+        glVertex3f(-size /4, -size / 2, size / 4);
+    glEnd();
+
+
+    // draw circle on negative x-axis side of the main cube
+    float PI = 3.14159;
+    glColor3f(1, 0.5, 0);
+    glBegin(GL_TRIANGLE_FAN);
+        for (int i = 0; i < 10; ++i)
+        {
+            glVertex3f(
+                    -size / 2, 
+                    std::cos(2 * i * PI / 10) * 0.7,
+                    std::sin(2 * i * PI / 10) * 0.7
+                    );
+        }
+    glEnd();
+    
+    // draw triangle that lies on x-axis side of the main cube
+    glColor3f(1, 0, 0);
+    glBegin(GL_POLYGON);
+        glVertex3f(size / 2, -size/4, size/4);
+        glVertex3f(size / 2, -size/4, -size/4);
+        glVertex3f(size / 2, size/2, 0);
+    glEnd();
+
+
     glPushMatrix();
-        glColor3f(1, 1, 0);
         glColor3f(0.9, 0.0, 0.2);
-        //glutWireCube(2.0);
-        glutSolidCube(2.0);
+        glutWireCube(size);
+        //glutSolidCube(2.0);
         coordinate_axes(1);
 
     glPopMatrix();
--- src/figures.h
+++ src/figures.h
@@ -1,7 +1,7 @@
 #ifndef FIGURES
 
 void coordinate_axes(int len);
-void draw_main_cube();
+void draw_main_cube(float size);
 void draw_falling_figure(int id);
 
 #define FIGURES 
